054bb9d884d062d002568c1ce20b6df151a951dd
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2024-10-25 17:34:31 +0200

    implement method for generating a random color

--- script.js
+++ script.js
@@ -30,3 +30,13 @@ function generateSquare() {
     square.classList.add("square");
     return square;
 }
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    console.log(color);
+    return color;
+}
