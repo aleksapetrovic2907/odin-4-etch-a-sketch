efc775f523ad0ad8ea899d1dd6e996349e5e4764
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2024-10-25 17:03:42 +0200

    implement logic for generating grid

---
+++ script.js
@@ -0,0 +1,20 @@
+const DEFAULT_GRID_SIZE = 16;
+
+document.addEventListener("DOMContentLoaded", initialize);
+
+function initialize() {
+    const grid = document.getElementById("grid");
+    generateGrid(grid, DEFAULT_GRID_SIZE);
+}
+
+function generateGrid(grid, gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        grid.appendChild(generateSquare());
+    }
+}
+
+function generateSquare() {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    return square;
+}
